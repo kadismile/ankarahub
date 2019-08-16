2a30b82d5906b3ba971a25397b873f990bd74813
Ibrahim Abubakar Sediq <ibrahim@kadismile.com>
2019-08-16 08:14:58 +0100

    fixed product detail page

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,13 +2,15 @@ import { combineReducers } from 'redux'
 import auth  from './authReducer'
 import category from './categoryReducer'
 import product from './productReducer'
+import singleProduct from './singleProduct'
 import cart from './cartReducer'
 
 export default combineReducers({
   auth,
   category,
   product,
-  cart
+  cart,
+  singleProduct
 })
 
 
